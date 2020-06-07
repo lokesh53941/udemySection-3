3c70c327f96b3f4c4f0c00b265d340185b748023
lokesh53941 <65780262+lokesh53941@users.noreply.github.com>
2020-06-07 17:37:38 +0530

    Create calculatingMean.py

---
+++ calculatingMean.py
@@ -0,0 +1,6 @@
+monday_temperatures=[9.1,8.8,7.5]
+student_grades={"marry" :9.1 ,"sin" :8.8, "jonh":7.5}
+mysum=sum(student_grades.values())
+length=len(student_grades)
+mean=mysum/length
+print(mean)
